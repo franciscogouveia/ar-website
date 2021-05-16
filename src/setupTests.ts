e60f0c1ecaf83baa7636f7c0350737bad3de9a69
Francisco de Gouveia <francisco@de-gouveia.eu>
2021-05-16 13:54:23 +0200

    Fixed testing issues

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+function noOp () { }
+if (typeof window.URL.createObjectURL === 'undefined') {
+    Object.defineProperty(window.URL, 'createObjectURL', { value: noOp})
+}
